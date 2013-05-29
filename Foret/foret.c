eba737097c2c1552d3caeb50d556e47d872c5133
Hakim <hakim@The-Notebook.(none)>
2013-05-29 20:46:24 +0200

    first commit

---
+++ Foret/foret.c
@@ -0,0 +1,23 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int sock ;
+struct sockaddr_in recv_addr ;
+
+int ouvertureLiaison(){
+	sock = socket (PF_INET, SOCK_DGRAM, 0) ;//creation d'une liaison
+	bzero ((char *) &recv_addr, sizeof recv_addr) ;//mise a zero de l'adresse de reception
+	recv_addr.sin_family = AF_INET ;
+	recv_addr.sin_addr.s_addr = htonl (INADDR_ANY) ;
+	recv_addr.sin_port = htons (5000) ;
+	bind (sock, (struct sockaddr *)&recv_addr, sizeof(recv_addr)) ;//assigne une adresse a sock 
+}
+
+int main(){
+	
+}
